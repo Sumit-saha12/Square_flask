a6978bdc56ad76439615e6236abf98dc9f903c0d
SUMIT SAHA <102068600+Sumit-saha12@users.noreply.github.com>
2024-08-24 19:09:23 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask,request,render_template
+app = Flask(__name__)
+
+@app.route('/square',methods=['GET','POST'])
+def square():
+    if request.method=="POST":
+        number = int(request.form['num'])
+        return render_template('square.html',number=number)
+    return render_template('form.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
